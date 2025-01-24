b7bbdda979c2a83b620c4bf3f7ab7991d9857855
saniayudin <olex.yudin@gmail.com>
2025-01-24 23:06:35 +0200

    Update

--- src/test/java/org/example/carService/pages/NewCarPage.java
+++ src/test/java/org/example/carService/pages/NewCarPage.java
@@ -5,21 +5,34 @@ import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
 import lombok.Getter;
 
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @Getter
 public class NewCarPage extends AbstractBasePage {
-
     private static final String PAGE_URL = "http://localhost:8080/carservice/clients/%s/cars/add";
     private final String pageName = "Create New Car";
     private final String headerValue = "Нове авто";
     private final String successMessageValue = "A new car added";
     private final Page playwrightPage = newPage();
     private final Locator headerLocator = playwrightPage.locator("//h2[text() = 'Нове авто']");
+
     private final Locator carNumberInput = playwrightPage.locator("input[name='number']");
+    private final Locator carNumberLabel = playwrightPage.locator("//label[text()='Номер авто']");
+    private final Locator carNumberPlaceHolder = playwrightPage.locator("//*[@id='number']");
+
     private final Locator vinCodeInput = playwrightPage.locator("input[name='vin_code']");
+    private final Locator vinCodeLabel = playwrightPage.locator("//label[text() = 'Він код']");
+    private final Locator vinCodePlaceholder = playwrightPage.locator("//*[@id= 'vin_code']");
+
     private final Locator brandInput = playwrightPage.locator("input[name='brand']");
+    private final Locator brandLabel = playwrightPage.locator("//label[text() = 'Бренд']");
+    private final Locator brandPlaceholder = playwrightPage.locator("//*[@id= 'brand']");
+
     private final Locator modelInput = playwrightPage.locator("input[name='model']");
+    private final Locator modelLabel = playwrightPage.locator("//label[text() = 'Модель']");
+    private final Locator modelPlaceholder = playwrightPage.locator("//*[@id= 'model']");
+
     private final Locator addButton = playwrightPage.locator("button[type='submit']");
     private final Locator successMessageLocator = playwrightPage.locator("//div[@class='alert alert-success']");
 
@@ -33,6 +46,8 @@ public class NewCarPage extends AbstractBasePage {
 
     @Step("Entering the car number: {carNumber}")
     public NewCarPage enterCarNumber(String carNumber) {
+        assertThat(carNumberLabel).isVisible();
+        assertThat(carNumberPlaceHolder).isVisible();
         carNumberInput.fill(carNumber);
         assertThat(carNumberInput).hasValue(carNumber);
         return this;
@@ -40,6 +55,8 @@ public class NewCarPage extends AbstractBasePage {
 
     @Step("Entering the vin code: {vinCode}")
     public NewCarPage enterCarVinCode(String vinCode) {
+        assertThat(vinCodeLabel).isVisible();
+        assertThat(vinCodePlaceholder).isVisible();
         vinCodeInput.fill(vinCode);
         assertThat(vinCodeInput).hasValue(vinCode);
         return this;
@@ -47,6 +64,8 @@ public class NewCarPage extends AbstractBasePage {
 
     @Step("Entering the brand value: {brand}")
     public NewCarPage enterCarBrand(String brand) {
+        assertThat(brandLabel).isVisible();
+        assertThat(brandPlaceholder).isVisible();
         brandInput.fill(brand);
         assertThat(brandInput).hasValue(brand);
         return this;
@@ -54,6 +73,8 @@ public class NewCarPage extends AbstractBasePage {
 
     @Step("Entering the model value: {model}")
     public NewCarPage enterCarModel(String model) {
+        assertThat(modelLabel).isVisible();
+        assertThat(modelPlaceholder).isVisible();
         modelInput.fill(model);
         assertThat(modelInput).hasValue(model);
         return this;
